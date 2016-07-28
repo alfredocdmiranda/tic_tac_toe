b86b6bfb886e56c61acc66f7ff96f4f24cf8de4b
Alfredo Miranda <alfredo.miranda@hp.com>
2016-07-28 17:41:04 -0300

    Finished user interface

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,8 @@
 class Table(object):
-    def __init__(self, width, heigth):
-        self.state = [[None for j in range(width)] for i in range(heigth)]
+    def __init__(self, size):
+        self.width = size
+        self.heigth = size
+        self.state = [[None for j in range(size)] for i in range(size)]
 
     def play(self, x, y, sym):
         if self.state[x][y] is None:
@@ -33,14 +35,26 @@ class Player(object):
         self.sym = sym
 
 def judge(table):
-    pass
+    result = 0
+
+    return result
 
 if __name__ == '__main__':
-    t = Table(3,3)
+    t = Table(3)
     players = [Player('P1', 'x'), Player('P2', 'o')]
     turn = 0
-    while not judge(t):
+    while True:
         x = int(input('X: '))
         y = int(input('Y: '))
-        t.play(x,y,players[turn])
-        
+        t.play(x,y,players[turn].sym)
+        result = judge(t)
+        if result > 0:
+            break
+        print(t)
+        print("-----------------------------")
+        turn = 1 if turn == 0 else 0
+
+    if result == 1: #WIN
+        print("{} WINS!".format(players[turn].name))
+    elif result == 2: #DRAW
+        print("DRAW")
